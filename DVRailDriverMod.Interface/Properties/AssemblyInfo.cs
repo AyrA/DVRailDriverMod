e514a4963d40d09ac23671b4a79d721250fbb0dd
Kevin Gut <kevin.gut@bitm.sg>
2023-05-21 01:30:33 +0200

    Update version

--- DVRailDriverMod.Interface/Properties/AssemblyInfo.cs
+++ DVRailDriverMod.Interface/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DVRailDriverMod.Interface")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utilities to interface using the RailDriver controller in Derail Valley")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AyrA")]
 [assembly: AssemblyProduct("DVRailDriverMod.Interface")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © AyrA 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
